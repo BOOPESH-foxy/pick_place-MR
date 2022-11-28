dc98f1599bb0c04507aea9501206c9a9552cd8fb
Boopesh <105548657+BOOPESH-foxy@users.noreply.github.com>
2022-11-29 01:29:16 +0530

    Delete build directory

--- build/iiwa_stack/iiwa_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/kuka_iiwa/src/iiwa_stack/iiwa_moveit
-# Build directory: /home/user/kuka_iiwa/build/iiwa_stack/iiwa_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/iiwa_stack/iiwa_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/kuka_iiwa/src/iiwa_stack/iiwa_ros
-# Build directory: /home/user/kuka_iiwa/build/iiwa_stack/iiwa_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
